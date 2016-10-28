9fda84373e93c8805d910b0df5d3a32bd1f11618
Anna Kviese <anna.kviese@gmail.com>
2016-10-28 13:51:21 -0700

    ball starting from the center

--- src/Ball.js
+++ src/Ball.js
@@ -2,27 +2,48 @@ export default class Ball {
     constructor(boardHeight, boardWidth, color, size){
         this.boardHeight = boardHeight;
         this.boardWidth = boardWidth;
-        this.x = this.boardHeight / 2;
-        this.y = this.boardWidth / 2;
+        
         this.vy = Math.floor(Math.random() * 12 - 6); // y direction
         this.vx = (7 - Math.abs(this.vy)); // x direction
         this.size = size;
         this.speed = 5;
+        this.reset()
+    }
+    reset() {
+        this.x = this.boardWidth / 2;
+        this.y = this.boardHeight / 2;
     }
 
-    draw (context) {
-    context.fillStyle = 'white';
-    context.beginPath();
-    context.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    context.fill();
-    context.closePath();
+        draw (context) {
+        context.fillStyle = 'white';
+        context.beginPath();
+        context.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        context.fill();
+        context.closePath();
   }
 
-    render(context) {
-        this.x += this.vx;
-        this.y += this.vy;
-        this.draw(context)
+    
 
+paddleCollision(p1, p2){
+        if (this.vx > 0) {
+            const inRightEnd = this.x >= p2.x;
+            if (inRightEnd) {
+                if (this.y >= p2.y && this.y <= (this.y + p2.height)){
+                    this.vx *= -1;
+                }
+            }
+        } else {
+            const inLeftEnd = this.x <= p1.x + p1.width
+            
+            if (inLeftEnd) {
+                if (this.y >= p1.y && this.y <= (this.y + p1.height)) {
+                    this.vx *= -1;
+                }
+            } 
+        } 
+   }
+
+   render(context, p1, p2) {
         const hitLeft = this.x >= this.boardWidth;
         const hitRight = this.x + this.size <= 0;
         const hitTop = this.y + this.size <= 0;
@@ -34,6 +55,11 @@ export default class Ball {
         if (hitTop || hitBottom){
             this.vy *= -1
         }
-      //...
+
+        this.x += this.vx;
+        this.y += this.vy;
+
+        this.paddleCollision(p1, p2)
+        this.draw(context)
     }
-    }
+}
--- src/Game.js
+++ src/Game.js
@@ -23,6 +23,6 @@ export default class Game {
         this.p1.render(this.context)
         this.p2.render(this.context)
         // this.ball.render(this.context)
-        this.ball.render(this.context, this.player1, this.player2);
+        this.ball.render(this.context, this.p1, this.p2);
 	}
 }
